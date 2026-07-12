cd8aaf0ad0cc0929880eb94bc2d9cfed8039ac17
mayanpatidar2122-source <mayanpatidar2122@gmail.com>
2026-07-12 10:12:23 +0530

    Add POST request support

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+        keyword = ""
+
+    if request.method == "POST":
+        keyword = request.form.get("keyword", "")
+
+    return render_template(
+        "index.html",
+        keyword=keyword
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
