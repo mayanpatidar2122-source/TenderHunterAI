ffb907b93cc1c1699d3c632f9420ebeba0d2b814
mayanpatidar2122-source <mayanpatidar2122@gmail.com>
2026-07-12 10:52:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-        keyword = ""
+    keyword = ""
 
     if request.method == "POST":
         keyword = request.form.get("keyword", "")
 
-    return render_template(
-        "index.html",
-        keyword=keyword
-    )
+    return render_template("index.html", keyword=keyword)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
